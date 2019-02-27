08c743aa5ac52e9bff3db521ad91ea9625da1a6f
Stacy Hanawa <stacyhanawa@gmail.com>
2019-02-26 23:26:18 -0800

    completed auto-discovery of content files

--- build.py
+++ build.py
@@ -4,19 +4,26 @@ print(all_html_files)
 
 import os
 
-file_path = "content/blog.html"
-file_name = os.path.basename(file_path)
-print(file_name)
-name_only, extension = os.path.splitext(file_name)
-print(name_only)
+def convert(file_path):
+    file_name = os.path.basename(file_path)
+    page_title, extension = os.path.splitext(file_name)
+    output = "docs/" + file_name
+    return {
+                "filename": file_path,
+                "title": page_title,
+                "output": output
+            }
 
 pages = []
-pages.append({
-    "filename": "content/index.html",
-    "title": "Index",
-    "output": "doc/index.html",
-})
+
+for files in all_html_files:
+    result = convert(files)
+    pages.append(result)
 print(pages)
+
+
+
+
 
 def apply_template(template, page_title, file_name):
     index_content = open(file_name).read()
