48faf237f7196126b07cdcb4d7cb2a6be3b51261
Stacy Hanawa <stacyhanawa@gmail.com>
2019-03-01 00:44:13 -0800

    created command-line arguments for manage.py

--- manage.py
+++ manage.py
@@ -1,14 +1,19 @@
 import utils
 
 import sys
-print("This is argv:", sys.argv)
-command = sys.argv[1]
-print(command)
-if command == "build":
-    print("Build was specified")
-    utils.main()
-elif command == "new":
-    print("New page was specified")
-    #
+
+if len(sys.argv) ==2:
+    command = sys.argv[1]
+    if command == "build":
+        print("Build was specified")
+        utils.main()
+    elif command == "new":
+        print("New page was specified")
+        text = "<h1>New Content!<h1><p>New content<p>"
+        open("content/new_content_page.html", "w+").write(text)
+    else:
+        print("Please specify 'build' or 'new'")
 else:
-    print("Please specify 'build' or 'new'")
+    print('''Usage:
+Rebuild site: python3 manage.py build
+Create new page: python3 manage.py new''')
